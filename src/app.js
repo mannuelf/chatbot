8e839f50ca61e4ee059ce60eb171da444d3bec77
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-23 20:08:28 +0200

    app secrets and configs

--- src/app.js
+++ src/app.js
@@ -8,6 +8,24 @@ const request = require('request')
 const app = express()
 const uuid = require('uuid')
 
+// Facebook messenger API
+if (! config.FB_PAGE_TOKEN) {
+    throw new Error('missing FB_PAGE_TOKEN')
+}
+
+if (! config.FB_VERIFY_TOKEN) {
+    throw new Error('missing FB_VERIFY_TOKEN')
+}
+
+if (! config.FB_APP_SECRET) {
+    throw new Error('missing FB_APP_SECRET')
+}
+
+if (! config.SERVER_URL) {
+    throw new Error('missing SERVER_URL')
+}
+
+// express server
 app.set('port', (process.env.PORT || 5000))
 
 app.listen(app.get('port'), () => {
