91f1217f58fab3c6c0f6d73a2ffc332ae3966c79
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-06-23 16:25:02 +0200

    require stuff

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
+'use_strict'
+
 const apiai = require('apiai')
+const config = require('../src/config')
+const express = require('express')
+const crypto = require('crypto')
+const bodyParser = require('request')
+const request = require('request')
+const app = express()
+const uuid = require('uuid')
+
+
