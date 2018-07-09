9c432e675a9348fda9732c5d3b2bef19a5524f11
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2018-07-09 21:54:57 +0200

    check if values are not empty and send email with content

--- src/app.js
+++ src/app.js
@@ -189,10 +189,39 @@ function handleApiAiAction(sender, action, responseText, contexts, parameters) {
                     ? contexts[0].parameters['phone-number']
                     : '');
 
-                let user_name = (isDefined(contexts[0].parameters['phone-number'])
-                    && contexts[0].parameters['phone-number'] != '' 
-                    ? contexts[0].parameters['phone-number']
+                let user_name = (isDefined(contexts[0].parameters['user-name'])
+                    && contexts[0].parameters['user-name'] != '' 
+                    ? contexts[0].parameters['user-name']
                     : '');
+                    
+                let previous_job = (isDefined(contexts[0].parameters['prevous-job'])
+                    && contexts[0].parameters['prevous-job'] != '' 
+                    ? contexts[0].parameters['prevous-job']
+                    : '');
+                    
+                let years_of_experience = (isDefined(contexts[0].parameters['prevous-job'])
+                    && contexts[0].parameters['prevous-job'] != '' 
+                    ? contexts[0].parameters['prevous-job']
+                    : '');
+                    
+                let job_vacancy = (isDefined(contexts[0].parameters['job-vacancy'])
+                    && contexts[0].parameters['job-vacancy'] != '' 
+                    ? contexts[0].parameters['job-vacancy']
+                    : '');
+
+                if (phone_number != ''
+                    && user_name != '' 
+                    && previous_job != '' 
+                    && years_of_experience != ''
+                    && job_vacancy != '') {
+                        let emailContent = 'A new job enquiry' 
+                            + user_name + ' for the job: ' + job_vacancy
+                            + '<br/> Previous job position: ' + previous_job
+                            + '<br/> Years of experience: ' + previous_job
+                            + '<br/> Phone number: ' + phone_number + '.';
+                        
+                        sendEmail('New Job Application', emailContent);
+                }
             }
             sendTextMessage(sender, responseText);
             break;
